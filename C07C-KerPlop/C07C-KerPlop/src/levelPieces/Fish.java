333b9586a69c46d56f0531b1d62251e38769c038
sgroth88 <sgroth@mines.edu>
2025-02-06 15:00:38 -0700

    Update Fish.java

--- C07C-KerPlop/C07C-KerPlop/src/levelPieces/Fish.java
+++ C07C-KerPlop/C07C-KerPlop/src/levelPieces/Fish.java
@@ -1,7 +1,38 @@
+package levelPieces
+
+import gameEngine.Drawable;
+
 public class Fish extends GamePiece implements Moveable {
-	
+
 // Constructor for Fish
 	public Fish(char symbol, String label, int location) {
 		super(symbol, label, location);
 	}
+	
+	public void move(Drawable[] gameBoard, int playerLocation) {
+			
+			if (direction == 0 && (this.getLocation() + 2) < GameEngine.BOARD_SIZE) {
+				this.setLocation(this.getLocation() + 2);
+			} else if (direction == 0 && ((this.getLocation() + 2) == (GameEngine.BOARD_SIZE - 1))) {
+				this.setLocation(this.getLocation() + 2);
+				this.direction = 0;
+			} else if (direction == 0 && ((this.getLocation() + 2) >= GameEngine.BOARD_SIZE)) {
+				direction = 1;
+				this.setLocation(this.getLocation() - 2);
+				
+			} else if (direction == 1 && (this.getLocation() - 2) > 0) {
+				this.setLocation(this.getLocation() - 2);
+			} else if (direction == 1 && ((this.getLocation() - 2) == 0)) {
+				this.setLocation(this.getLocation() - 2);
+				direction = 0;
+			} else if (direction == 1 && ((this.getLocation() - 2) < 0)) {
+				direction = 0;
+				this.setLocation(this.getLocation() + 2);
+			}
+			System.out.println(this.getLocation());
+		}
+	
+	public InteractionResult interact() {
+			return InteractionResult.NONE;
+		}
 }
