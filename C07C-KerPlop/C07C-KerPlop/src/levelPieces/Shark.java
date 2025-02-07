9f74f9d49f7f0231f3bf61ec770ec0673626496f
gdematta <gdematta@mines.edu>
2025-02-06 19:29:36 -0700

    Final Commit

--- C07C-KerPlop/C07C-KerPlop/src/levelPieces/Shark.java
+++ C07C-KerPlop/C07C-KerPlop/src/levelPieces/Shark.java
@@ -18,25 +18,47 @@ public class Shark extends GamePiece implements Moveable {
 	
 	public void move(Drawable[] gameBoard, int playerLocation) {
 		
-		if (direction == 0 && (this.getLocation() + 2) < GameEngine.BOARD_SIZE) {
+		int init = this.getLocation();
+		//Facing Right conditions:
+		// If facing right and valid location, move right 2
+		if (direction == 0 && (this.getLocation() + 2) < GameEngine.BOARD_SIZE && gameBoard[this.getLocation() + 2] == null) {
 			this.setLocation(this.getLocation() + 2);
-		} else if (direction == 0 && ((this.getLocation() + 2) == (GameEngine.BOARD_SIZE - 1))) {
+		// If facing right and valid location but at the edge, move right 2 and change directions
+		} else if (direction == 0 && ((this.getLocation() + 2) == (GameEngine.BOARD_SIZE - 1)) && gameBoard[this.getLocation() + 2] == null) {
 			this.setLocation(this.getLocation() + 2);
-			this.direction = 0;
-		} else if (direction == 0 && ((this.getLocation() + 2) >= GameEngine.BOARD_SIZE)) {
+			this.direction = 1;
+		// If facing right and invalid location, move the remaining spaces and flip direction
+		} else if (direction == 0 && ((this.getLocation() + 2) >= GameEngine.BOARD_SIZE) && gameBoard[this.getLocation() + 1] == null) {
+			this.setLocation(this.getLocation() + 1);
 			direction = 1;
-			this.setLocation(this.getLocation() - 2);
+		} else if (direction == 0 && gameBoard[this.getLocation() + 1] == null) {
+			this.setLocation(this.getLocation() + 1);
+			direction = 1;
+		} else if (direction == 0) {
+			direction = 1;
 			
-		} else if (direction == 1 && (this.getLocation() - 2) > 0) {
+			
+		//Facing Right conditions:
+		// If facing left and valid location, move right 2
+		}	else if (direction == 1 && (this.getLocation() - 2) > 0 && gameBoard[this.getLocation() - 2] == null) {
 			this.setLocation(this.getLocation() - 2);
-		} else if (direction == 1 && ((this.getLocation() - 2) == 0)) {
+		// If facing left and valid location but at the edge, move right 2 and change directions
+		} else if (direction == 1 && ((this.getLocation() - 2) == 0) && gameBoard[this.getLocation() - 2] == null) {
 			this.setLocation(this.getLocation() - 2);
+			this.direction = 0;
+		// If facing left and invalid location, move the remaining spaces and flip direction
+		} else if (direction == 1 && ((this.getLocation() - 2) < 0) && gameBoard[this.getLocation() - 1] == null) {
+			this.setLocation(this.getLocation() - 1);
 			direction = 0;
-		} else if (direction == 1 && ((this.getLocation() - 2) < 0)) {
+		} else if (direction == 0 && gameBoard[this.getLocation() - 1] == null) {
+			this.setLocation(this.getLocation() - 1);
 			direction = 0;
-			this.setLocation(this.getLocation() + 2);
+		} else if (direction == 1) {
+			direction = 0;
 		}
-		System.out.println(this.getLocation());
+		
+		gameBoard[init] = null;
+		gameBoard[this.getLocation()] = this;
 	}
 	
 	public InteractionResult interact(Drawable [] gameBoard, int playerLocation) {
